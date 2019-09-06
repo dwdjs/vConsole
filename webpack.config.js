a6b7818373cfb8e0d430250214949c3e444aba85
jack <1395093509@qq.com>
2019-09-06 16:16:56 +0800

    feat: 新增 reload 按钮

--- webpack.config.js
+++ webpack.config.js
@@ -21,7 +21,7 @@ module.exports = {
       {
         test: /\.html$/, loader: 'html-loader?minimize=false'
       },
-      { 
+      {
         test: /\.js$/, loader: 'babel-loader'
       },
       {
